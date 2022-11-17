efec65d137d6e218a438ba58c9a0f4b9faa00420
shihan-syed <shihan.s@iinerds.com>
2022-11-17 12:29:51 +0530

    mongodb connected

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 const express = require('express')
+const dotenv =require("dotenv")
+const mongoose =require('mongoose')
 const app= express()
+dotenv.config()
 
-app.listen(4000,()=>{console.log(`connectedto backend`)})
+const connect = async ()=>{
+   try {
+    await mongoose.connect(process.env.MONGO)
+    console.log('connected to mongoDB');
+  } catch (error) {
+    throw error
+  } 
+}
+
+mongoose.connection.on("connected",()=>{console.log('mongoDB connected')})
+mongoose.connection.on("disconnected",()=>{console.log('mongoDB disconnected')})
+
+
+app.listen(4000,()=>{
+    connect()
+    console.log(`connected to backend`)})
